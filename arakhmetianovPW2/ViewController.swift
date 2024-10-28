f4f71872c37ad9d1b027dd8ee2f0d6a75c3311e6
Askar <Askar.Akhmetyanov@yandex.ru>
2024-10-28 18:37:21 +0300

    Initial Commit

---
+++ arakhmetianovPW2/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  arakhmetianovPW2
+//
+//  Created by Аскар Ахметьянов on 28.10.2024.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
